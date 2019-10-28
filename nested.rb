4226254ba677fb3f674c5a4a6e9a6caaad2de500
Eric Manning <emanningmusic@yahoo.com>
2019-10-28 20:52:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
- programmer_hash.each do |name , val|
-   name.each 
+ 
  end
    
  end
